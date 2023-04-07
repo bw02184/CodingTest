2d30c7ba63e0d7a1b6a1640fdca343df72c07b32
bw02184 <bw02184@naver.com>
2023-04-07 14:36:37 +0900

    프로그래머스2

---
+++ src/이진변환반복하기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] solution(String s) {
+        int[] answer = new int[2];
+        while(!s.equals("1")){
+            int length = s.length();
+            int zCount=0;
+            for(int i=0;i<length; i++){
+                if(s.charAt(i)=='0') zCount++;
+            }
+            s = Integer.toBinaryString(length-zCount);
+            answer[0]++;
+            answer[1] += zCount;
+        }
+        return answer;
+    }
+}
