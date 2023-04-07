542970d738571116cc4cc8c4c6e21f85f039fc8e
bw02184 <bw02184@naver.com>
2023-04-07 15:24:04 +0900

    프로그래머스2 -- 효율성 테스트 성공

--- src/숫자의표현.java
+++ src/숫자의표현.java
@@ -3,19 +3,16 @@ class Solution {
     public int solution(int n) {
         int answer = 0;
         int[] array = new int[n+1];
-        for(int i=1; i<n+1; i++){
-            array[i] = array[i-1]+i;
-        }
-        System.out.println(Arrays.toString(array));
-        int first = 0, second = 1;
-        while(second<=n){
-            if(array[second] - array[first] < n){
-                second++;
-            }else if(array[second] - array[first] == n){
+        int left = 0, right = 1;
+        int sum=right - left;
+        while(right<=n){
+            if(sum < n){
+                sum += ++right;
+            }else if(sum == n){
                 answer++;
-                second++;
+                sum += ++right;
             }else
-                first++;
+                sum -= ++left;
         }
 
         return answer;
