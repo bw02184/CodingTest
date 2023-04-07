9abeb32a6ef2d3504448a999180b21763ba90c7f
bw02184 <bw02184@naver.com>
2023-04-07 15:36:29 +0900

    프로그래머스2

---
+++ src/다음큰숫자.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        int nOneCount = getOneCount(n);
+        int otherOneCount = getOneCount(++n);
+        while(nOneCount != otherOneCount){
+            otherOneCount = getOneCount(++n);
+        }
+        return n;
+    }
+
+    public int getOneCount(int n){
+        int count=0;
+        String bin = Integer.toBinaryString(n);
+        for(int i=0; i<bin.length(); i++){
+            if(bin.charAt(i)=='1') count++;
+        }
+        return count;
+    }
+}
