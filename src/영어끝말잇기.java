0f1808169c520db0b689d16fd7302728c31234cb
bw02184 <bw02184@naver.com>
2023-04-09 17:49:53 +0900

    프로그래머스2

---
+++ src/영어끝말잇기.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int n, String[] words) {
+        int[] answer = new int[2];
+        HashSet<String> set = new HashSet<>();
+        int size=0;
+        String bfString = words[0];
+        set.add(words[0]);
+        for(int i=1; i<words.length; i++){
+            set.add(words[i]);
+            if(set.size() != i+1 || bfString.charAt(bfString.length()-1) != words[i].charAt(0)){
+                size = i+1;
+                break;
+            }
+            bfString = words[i];
+        }
+        answer[0] = size%n ==0 && size !=0? n : size%n;
+        answer[1] = (int)Math.ceil(size*1.0/n);
+        return answer;
+    }
+}
