cf35f9d32daddefd87128a27c4316179ee1aaf99
bw02184 <bw02184@naver.com>
2023-04-10 21:34:28 +0900

    프로그래머스1 -- HashMap 이용

---
+++ src/대충만든자판.java
@@ -0,0 +1,30 @@
+import java.util.HashMap;
+class Solution {
+    public int[] solution(String[] keymap, String[] targets) {
+        int[] answer = new int[targets.length];
+        HashMap<String,Integer> map = new HashMap<>();
+        for(int i=0; i<keymap.length; i++){
+            String[] s=keymap[i].split("");
+            for(int j=0; j<keymap[i].length(); j++){
+                if(map.getOrDefault(s[j], 101) >j) map.put(s[j],j+1);
+
+            }
+        }
+        System.out.println(map);
+        int index=0;
+        for(String s : targets){
+            int sum=0;
+            for(int i=0; i<s.length(); i++){
+                if(map.get(""+s.charAt(i)) == null){
+                    sum = -1;
+                    break;
+                }
+                else
+                    sum +=map.get(""+s.charAt(i));
+            }
+            answer[index] = sum;
+            index++;
+        }
+        return answer;
+    }
+}
