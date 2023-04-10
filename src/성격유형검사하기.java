f0c1ac121612c7a4c5f8d597dcc8d9f1cac9a4b3
bw02184 <bw02184@naver.com>
2023-04-10 20:50:47 +0900

    프로그래머스1 -- HashMap 이용

---
+++ src/성격유형검사하기.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+class Solution {
+    public String solution(String[] survey, int[] choices) {
+        String[] type = {"RT","CF","JM","AN"};
+        String answer = "";
+        HashMap<Character,Integer> map = new HashMap<>();
+        for(int i=0; i<survey.length;i++){
+            int zero = map.getOrDefault(survey[i].charAt(0),0);
+            int first = map.getOrDefault(survey[i].charAt(1),0);
+            map.put(survey[i].charAt(0),
+                choices[i]>=4?0 + zero : 4-choices[i] + zero);
+            map.put(survey[i].charAt(1),
+                choices[i]>=4?choices[i]-4 + first : 0 +first);
+        }
+        for(int i=0; i<type.length; i++){
+            int zero = map.getOrDefault(type[i].charAt(0), 0);
+            int first = map.getOrDefault(type[i].charAt(1), 0);
+            if(zero == first)answer +=type[i].charAt(0);
+            else if(zero > first)answer +=type[i].charAt(0);
+            else answer +=type[i].charAt(1);
+        }
+        return answer;
+    }
+}
