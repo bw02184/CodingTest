613bd036afab44e7e80b23523ccf0d6a5b0dac58
bw02184 <bw02184@naver.com>
2023-04-10 13:26:52 +0900

    프로그래머스2 -- 그리디(투포인터 사용)

---
+++ src/구명보트.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+class Solution {
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+        Arrays.sort(people);
+        int left=0, right=people.length-1;
+        while(left <=right){
+            if(people[left] + people[right] > limit){
+                answer++;
+                right--;
+            }else if(people[left] + people[right] <=limit){
+                left++;
+                right--;
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
