76cd1e0dff46181410e8765909874bfc3cc28548
bw02184 <bw02184@naver.com>
2023-04-11 13:52:05 +0900

    프로그래머스1 -- HashMap 2개 이용

--- src/달리기경주.java
+++ src/달리기경주.java
@@ -3,19 +3,18 @@ class Solution {
     public String[] solution(String[] players, String[] callings) {
         String[] answer = new String[players.length];
         HashMap<String, Integer> map = new HashMap<>();
+        HashMap<Integer, String> map2 = new HashMap<>();
         for (int i = 0; i < players.length; i++) {
             map.put(players[i], i);
+            map2.put(i, players[i]);
         }
         for (int i = 0; i < callings.length; i++) {
-            String findKey = "";
-            for (String key : map.keySet()) {
-                if (map.get(key) == map.get(callings[i]) - 1) {
-                    findKey = key;
-                    break;
-                }
-            }
-            map.put(findKey, map.get(findKey) + 1);
-            map.put(callings[i], map.get(callings[i]) - 1);
+            int value = map.get(callings[i]);
+            String beforename = map2.get(value - 1);
+            map.put(callings[i], value - 1);
+            map.put(beforename, map.get(beforename) + 1);
+            map2.put(value - 1, callings[i]);
+            map2.put(value, beforename);
         }
         int index = 0;
         for (String key : map.keySet()) {
