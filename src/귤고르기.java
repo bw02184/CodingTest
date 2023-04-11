6472f32c74d4d30afa8de8e8909a0160fd927cf3
bw02184 <bw02184@naver.com>
2023-04-11 22:21:20 +0900

    프로그래머스2

---
+++ src/귤고르기.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+class Solution {
+    public int solution(int k, int[] tangerine) {
+        int answer = 0;
+        int[] array = new int[10000001];
+        for(Integer t : tangerine){
+            array[t] = array[t]+1;
+        }
+
+        Arrays.sort(array);
+        int sum=0;
+        int index = 1;
+        for(int i=array.length-1; i>0; i--){
+            sum +=array[i];
+            if(sum >= k) {
+                answer = index;
+                break;}
+            index++;
+        }
+        return answer;
+    }
+}
