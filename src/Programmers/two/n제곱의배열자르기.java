6cdce9e0cefc886f7ffbbeaf8ce397d0ec5bc420
bw02184 <bw02184@naver.com>
2023-04-14 21:46:36 +0900

    프로그래머스2 -- 정답 배낌

--- src/Programmers/two/n제곱의배열자르기.java
+++ src/Programmers/two/n제곱의배열자르기.java
@@ -1,22 +1,14 @@
-import java.util.*;
 class Solution {
-    public int[] solution(int n, long left, long right) {
-        int[] answer = new int[(int)(right-left+1)];
-        int[][] squard = new int[n][n];
-        int[] temp = new int[n*n];
-        int index=0;
-        for(int i=0; i<squard.length; i++){
-            for(int j=0; j<squard.length; j++){
-                if(j < i+1) squard[i][j] = i+1;
-                else squard[i][j] = j+1;
-                temp[index++] = squard[i][j];
-            }
+    public static int[] solution(int n, long left, long right) {
+
+        int[] answer = new int[(int)(right - left) + 1];
+        for (int i = 0; i < answer.length; i++) {
+            int y = (int)(left / n + 1);
+            int x = (int)(left % n + 1);
+            left++;
+            answer[i] = Math.max(y, x);
         }
-        index=0;
-        //System.out.println(Arrays.deepToString(squard));
-        //System.out.println(Arrays.toString(temp));
-        for(int i=(int)left; i<=right; i++)
-            answer[index++] = temp[i];
+
         return answer;
     }
 }
