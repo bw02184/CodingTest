a6c08b32c11d74ff7a2690fd662504166bc8b8f9
bw02184 <bw02184@naver.com>
2023-04-16 18:04:01 +0900

    1764번

--- src/BaekJoon/듣보잡.java
+++ src/BaekJoon/듣보잡.java
@@ -1,24 +1,35 @@
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 import java.util.StringTokenizer;
 
 class Main{
     public static void main(String[] args)throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int answer = 0;
-        ArrayList<String> list = new ArrayList<>();
+        HashSet<String> set = new HashSet<>();
+        List<String> list = new ArrayList<>();
         StringTokenizer st = new StringTokenizer(br.readLine().trim());
         int num1 = Integer.parseInt(st.nextToken());
         int num2 = Integer.parseInt(st.nextToken());
         for(int i=0; i<num1;i++)
-            list.add(br.readLine().trim());
+            set.add(br.readLine().trim());
         while(answer++<num2){
+            int bSize = set.size();
             String str = br.readLine().trim();
-            if(list.contains(str)){
-                System.out.println(str);
+            set.add(str);
+            if(bSize == set.size()){
+                list.add(str);
             }
         }
+        Collections.sort(list);
+        System.out.println(list.size());
+        for(String s : list)
+            System.out.println(s);
     }
 }
