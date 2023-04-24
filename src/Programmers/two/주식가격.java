dbef8082e857bea3984bbe725c3050c1d1706d8f
bw02184 <bw02184@naver.com>
2023-04-24 11:15:32 +0900

    프로그래머스2 -- 정답 배낌

--- src/Programmers/two/주식가격.java
+++ src/Programmers/two/주식가격.java
@@ -1,13 +1,13 @@
 class Solution {
     public int[] solution(int[] prices) {
         int[] answer = new int[prices.length];
-        for(int i=prices.length-1;i>0;i--){
-            for(int j=0;j<i;j++){
-                if(prices[j]<=prices[i]){
-                    answer[j]++;
-                }
-                else{
-                    answer[j]=1;
+
+        for(int i = 0; i < prices.length; i++) {
+            for(int j = i+1; j <prices.length; j++) {
+                answer[i] ++;
+                // 자신보다 작은 값이 없는 동안 반복
+                if(prices[i] > prices[j]) {
+                    break;
                 }
             }
         }
