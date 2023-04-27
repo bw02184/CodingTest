2c5a39aae88419d997beeb64f910ad22b2d1d2a2
bw02184 <bw02184@naver.com>
2023-04-27 15:41:05 +0900

    프로그래머스2 -- 스택 이용

--- src/Programmers/two/뒤에있는큰수찾기.java
+++ src/Programmers/two/뒤에있는큰수찾기.java
@@ -4,13 +4,12 @@ class Solution {
   public int[] solution(int[] numbers) {
     int[] answer = new int[numbers.length];
     Arrays.fill(answer, -1);
+    Stack<Integer> stack = new Stack<>();
     for (int i = 0; i < numbers.length; i++) {
-      for (int j = i + 1; j < numbers.length; j++) {
-        if (numbers[i] < numbers[j]) {
-          answer[i] = numbers[j];
-          break;
-        }
+      while (!stack.isEmpty() && numbers[stack.peek()] < numbers[i]) {
+        answer[stack.pop()] = numbers[i];
       }
+      stack.push(i);
     }
     return answer;
   }
