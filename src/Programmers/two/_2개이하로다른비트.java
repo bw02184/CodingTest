6708bfe404489bef6a8d2dc53928be84cafe3b09
bw02184 <bw02184@naver.com>
2023-04-27 18:35:54 +0900

    프로그래머스2

--- src/Programmers/two/_2개이하로다른비트.java
+++ src/Programmers/two/_2개이하로다른비트.java
@@ -7,20 +7,24 @@ class Solution {
       if (number % 2 == 0) answer[index++] = number + 1;
       else {
         StringBuilder sb = new StringBuilder();
-        while (number > 0) {
-          sb.append(number % 2);
-          number /= 2;
+        long number2 = number;
+        while (number2 > 0) {
+          sb.append(number2 % 2);
+          number2 /= 2;
         }
         sb.append(0);
         String sNum = sb.toString();
+        int temp = 0;
         for (int i = 0; i < sNum.length() - 1; i++) {
           if (sNum.charAt(i) == '1' && sNum.charAt(i + 1) == '0') {
             sb.setCharAt(i, '0');
             sb.setCharAt(i + 1, '1');
+            temp = i;
             break;
           }
         }
-        answer[index++] = Long.parseLong("" + Long.parseLong(sb.reverse().toString()), 2);
+        answer[index++] = number - (long) Math.pow(2, temp) + (long) Math.pow(2, temp + 1);
+        // answer[index++] = Long.parseLong(""+Long.parseLong(sb.reverse().toString()),2);
       }
     }
     return answer;
