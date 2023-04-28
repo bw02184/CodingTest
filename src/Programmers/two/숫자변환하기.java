4d937861141ff642f25c5c54194898fc46e1f25c
bw02184 <bw02184@naver.com>
2023-04-28 16:58:23 +0900

    프로그래머스2 - 정답배낌

--- src/Programmers/two/숫자변환하기.java
+++ src/Programmers/two/숫자변환하기.java
@@ -2,16 +2,21 @@ import java.util.*;
 
 class Solution {
   public int solution(int x, int y, int n) {
-    int answer = 0;
-    int[] dp = new int[1000001];
-    Arrays.fill(dp, -1);
-    int i = x;
-    dp[x] = 0;
-    while (i + n <= 1000000) {
-      i += n;
-      dp[i] = dp[i - n] + 1;
-      if (i % 3 == 0 && dp[i / 3] != -1) dp[i] = Math.min(dp[i], dp[i / 3] + 1);
-      if (i % 2 == 0 && dp[i / 2] != -1) dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+    int[] dp = new int[y + 1];
+    for (int i = x; i < y + 1; i++) {
+      if (i != x && dp[i] == 0) {
+        dp[i] = -1;
+        continue;
+      }
+      if (i * 2 <= y) {
+        dp[i * 2] = (dp[i * 2] == 0) ? dp[i] + 1 : Math.min(dp[i] + 1, dp[i * 2]);
+      }
+      if (i * 3 <= y) {
+        dp[i * 3] = (dp[i * 3] == 0) ? dp[i] + 1 : Math.min(dp[i] + 1, dp[i * 3]);
+      }
+      if (i + n <= y) {
+        dp[i + n] = (dp[i + n] == 0) ? dp[i] + 1 : Math.min(dp[i] + 1, dp[i + n]);
+      }
     }
     return dp[y];
   }
