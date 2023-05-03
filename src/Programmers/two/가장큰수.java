6ad8c6d649e7a88671ea424796791155c01a1065
bw02184 <bw02184@naver.com>
2023-05-03 13:57:54 +0900

    프로그래머스2 - 정답배낌

--- src/Programmers/two/가장큰수.java
+++ src/Programmers/two/가장큰수.java
@@ -2,56 +2,26 @@ import java.util.*;
 
 class Solution {
   public String solution(int[] numbers) {
-    String answer = "";
-    String[] temp = new String[numbers.length];
-    int index = 0;
-    Number[] num = new Number[numbers.length];
-    for (int n : numbers) {
-      String sNum = "" + n;
-      num[index] = new Number(n, sNum.length());
-      num[index].setbNum(num[index].changeNum(n));
-      index++;
-    }
-    // System.out.println(Arrays.toString(num));
-    Arrays.sort(num);
-    // System.out.println(Arrays.toString(num));
-    for (int i = num.length - 1; i >= 0; i--) answer += num[i].getNum();
-    return answer;
-  }
+    String[] stringNumbers = new String[numbers.length];
 
-  class Number implements Comparable<Number> {
-    int num;
-    int length;
-    String bNum;
-
-    public Number(int num, int length) {
-      this.num = num;
-      this.length = length;
+    // int형 배열을 String형 배열로 바꿈
+    for (int i = 0; i < numbers.length; i++) {
+      stringNumbers[i] = Integer.toString(numbers[i]);
     }
 
-    public void setbNum(String bNum) {
-      this.bNum = bNum;
-    }
+    // 두 수를 번갈아가며 합친 수 중 큰 값을 앞으로 정렬(내림차순 정렬)
+    Arrays.sort(
+        stringNumbers,
+        new Comparator<String>() {
+          @Override
+          public int compare(String o1, String o2) {
+            return (o2 + o1).compareTo(o1 + o2);
+          }
+        });
 
-    public int getNum() {
-      return this.num;
-    }
+    if (stringNumbers[0].equals("0")) return "0";
 
-    public String changeNum(int num) {
-      String sNum = "" + num;
-      while (sNum.length() < 3) {
-        sNum += sNum.charAt(0);
-      }
-      return sNum;
-    }
-
-    public String toString() {
-      return this.num + " " + this.length;
-    }
-
-    @Override
-    public int compareTo(Number o) {
-      return Integer.parseInt(this.bNum) - Integer.parseInt(o.bNum);
-    }
+    // 문자열을 delimiter("") 기준으로 합침
+    return String.join("", stringNumbers);
   }
 }
