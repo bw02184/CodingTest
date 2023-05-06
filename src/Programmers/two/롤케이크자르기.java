261c6ecb3c5451e0c9aa8d5376888be483e3e09c
bw02184 <bw02184@naver.com>
2023-05-06 23:41:57 +0900

    프로그래머스2

--- src/Programmers/two/롤케이크자르기.java
+++ src/Programmers/two/롤케이크자르기.java
@@ -4,17 +4,23 @@ class Solution {
   public int solution(int[] topping) {
     int answer = 0;
     int index = 0;
-    while (index < topping.length) {
-      HashSet<Integer> set1 = new HashSet<>();
-      HashSet<Integer> set2 = new HashSet<>();
-      for (int i = 0; i <= index; i++) {
-        set1.add(topping[i]);
-      }
-      for (int j = index + 1; j < topping.length; j++) {
-        set2.add(topping[j]);
-      }
-      if (set1.size() == set2.size()) answer++;
+    HashMap<Integer, Integer> map1 = new HashMap<>();
+    HashMap<Integer, Integer> map2 = new HashMap<>();
+    for (int i = 0; i <= index; i++) {
+      map1.put(topping[i], map1.getOrDefault(topping[i], 0) + 1);
+    }
+    for (int j = index + 1; j < topping.length; j++) {
+      map2.put(topping[j], map2.getOrDefault(topping[j], 0) + 1);
+    }
+    while (index < topping.length - 1) {
       index++;
+      map1.put(topping[index], map1.getOrDefault(topping[index], 0) + 1);
+      map2.put(topping[index], map2.getOrDefault(topping[index], 0) - 1);
+      if (map2.get(topping[index]) == 0) {
+        map2.remove(topping[index]);
+      }
+      if (map1.size() == map2.size()) answer++;
+      // System.out.println(map1.size()+" " +map2Count);
     }
     return answer;
   }
